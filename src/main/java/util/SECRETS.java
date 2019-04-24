ccc90ca89a80c2feb566484ebd2e92821a529ae8
PandorasPinata <32804354+PandorasPinata@users.noreply.github.com>
2019-04-24 13:52:36 +0200

    Update SECRETS.java

--- src/main/java/util/SECRETS.java
+++ src/main/java/util/SECRETS.java
@@ -2,5 +2,5 @@ package util;
 
 public class SECRETS {
 
-    public static final String TOKEN = "NTM0MDMzNTYxMDgzNDQ1MjQ5.DxztDQ.hon1tGaCl3Vki8tVK7sBcEEGEv4";
+    public static final String TOKEN = "Your Token Here";
 }
